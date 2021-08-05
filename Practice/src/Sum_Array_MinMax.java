1a370e428293ddd935828580011184c18b45fdce
Mudit Chauhan <mudittchauhan@gmail.com>
2021-08-04 23:13:23 -0400

    Min & Max value in array

---
+++ Practice/src/Sum_Array_MinMax.java
@@ -0,0 +1,35 @@
+class Sum_Array_MinMax
+{  
+	static void min(int m1[])
+	{
+		int min=m1[0];
+		for (int i=1;i<m1.length;i++)
+		{
+			if( min>m1[i])
+			{
+				min=m1[i];
+			}
+		}
+		System.out.println("Minimun value in array is "+ min);
+	}
+	
+	static void max(int m2[])
+	{
+		int max=m2[0];
+		for(int i=1;i<m2.length;i++)
+		{
+			if (max<m2[i])
+			{
+				max=m2[i];
+			}
+		}
+		System.out.println("Minimun value in array is "+ max);
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] arr = {33,25,44,12,60,4};
+		min(arr);
+		max(arr);		
+	}
+}  
