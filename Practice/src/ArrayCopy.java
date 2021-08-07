c3fd5f37b7ad82a917bc15484cd5d5775dce8b6c
Mudit Chauhan <mudittchauhan@gmail.com>
2021-08-07 00:30:42 -0400

    Single & 2D array copy using arraycopy & direct approach

--- Practice/src/ArrayCopy.java
+++ Practice/src/ArrayCopy.java
@@ -1,16 +1,34 @@
+import java.util.Arrays;
 
 public class ArrayCopy 
 {
 	public static void main(String[] args) 
 	{
+		// Copy single integer array using arraycopy method
 		int a[]= {1,2,3,4,5,6};
-		int b[]=new int[5];
-		System.arraycopy(a,1,b,0,5);
-		for(int i=0;i<b.length;i++)
-			{
-			System.out.print(b[i]);
-			}
-
+		int b[]=new int[a.length];
+		System.arraycopy(a,0,b,0,a.length);
+		System.out.println(Arrays.toString(b));
+		System.out.println();
+		
+		// Copy single string array using arraycopy method
+		String a1[]= {"a","b","c","d"};
+		String b1[]=new String[a1.length];
+		System.arraycopy(a1,0,b1,0,a1.length);
+		System.out.println(Arrays.toString(b1));
+		System.out.println();
+		
+		// Copy 2D integer array using direct approach
+		int[][] arr = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+		int[][] arrcopy = arr;
+		System.out.println(Arrays.deepToString(arrcopy));
+		System.out.println();
+		
+		// Copy 2D integer array using arraycopy
+		int[][] ar = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+		int[][] dest = new int[ar.length][];
+		System.arraycopy(ar,0,dest,0,ar.length);
+		System.out.println(Arrays.deepToString(dest));
 	}
 
 }
