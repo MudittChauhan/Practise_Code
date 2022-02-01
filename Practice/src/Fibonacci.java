ab1460125d11fd933327ef0d8f4991aaef1bbf6c
Mudit Chauhan <mudittchauhan@gmail.com>
2022-01-31 23:52:35 -0500

    Fibonacci with user input

--- Practice/src/Fibonacci.java
+++ Practice/src/Fibonacci.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
 
-public class Fibonacci {
-
-	public static void main(String[] args) 
+public class Fibonacci
+{
+	@SuppressWarnings("resource")
+	public static void main(String args[])
 	{
-	int num=10, n1=0,n2=1,n3;
-	System.out.print(n1+"  "+n2+"  ");
-	for(int i=2;i<num;i++)
-	{
-		n3=n1+n2;
-		System.out.print(n3+"  ");
-		n1=n2;
-		n2=n3;
+		int num1=0, num2=1, num3, count;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter number for Fibonacci length: ");
+		count=sc.nextInt();
+		System.out.print(num1+" "+num2);
+		for(int i=0; i<count-2; i++)
+		{
+			num3=num1+num2;
+			num1=num2;
+			num2=num3;
+			System.out.print(" "+num3);
+		}
 	}
-	}
-
 }
